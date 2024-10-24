c188891bc1c6dfc0986d68d418f50664ae7d7a5b
madhuri0908 <madhuri09081994@gmail.com>
2024-10-24 11:05:07 +0530

    Fetching title

--- MVN/src/test/java/com/actitime/testscript/CustomerModule.java
+++ MVN/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -1,11 +1,20 @@
 package com.actitime.testscript;
 
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class CustomerModule {
 	@Test
 public void createCustomer() {
-Reporter.log("createCustomer",true);
-}
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://wwww.google.com");
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.quit();
+	}
 }
